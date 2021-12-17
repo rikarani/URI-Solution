6e8e80d800507c3d5bef0ed56da70821fe04509d
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2021-12-17 23:11:08 +0700

    Add files via upload

---
+++ Python/1040.py
@@ -0,0 +1,27 @@
+N1, N2, N3, N4 = list(map(float, input().split()))
+exam = 0.0
+final_media = 0.0
+
+media = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10
+print(f"Media: {'{0:.1f}'.format(media)}")
+
+if media >= 7.0 :
+    print("Aluno aprovado.")
+
+if media < 5.0 :
+    print("Aluno reprovado.")
+
+if media >= 5.0 and media <= 6.9 :
+    print("Aluno em exame.")
+
+    exam = float(input())
+    print(f"Nota do exame: {'{0:.1f}'.format(exam)}")
+
+    final_media = (media + exam) / 2
+
+    if final_media >= 5.0 :
+        print("Aluno aprovado.")
+    else :
+        print("Aluno reprovado.")
+
+    print(f"Media final: {'{0:.1f}'.format(final_media)}")
