82a14905ff21808763ef4a8bc472540b056c75f6
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2021-12-18 00:00:09 +0700

    Add files via upload

---
+++ Python/1042.py
@@ -0,0 +1,13 @@
+A, B, C = list(map(int, input().split()))
+
+daftarNilai = [A,B,C]
+daftarNilai.sort()
+
+print(daftarNilai[0])
+print(daftarNilai[1])
+print(daftarNilai[2])
+
+print()
+print(A)
+print(B)
+print(C)
